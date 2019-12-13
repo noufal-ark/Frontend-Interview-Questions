6c0acbd8efceb4c973f4b2c95a626278c9185743
noufal-ark <noufal.mhdk@gmail.com>
2019-12-13 11:02:05 +0530

    Fix login and register style

--- src/app/pages/authPage/register/register.page.ts
+++ src/app/pages/authPage/register/register.page.ts
@@ -1,10 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
+  animations: [
+    trigger('openClose', [
+      transition('* => true', [
+        style({
+          height: '100px',
+          overflow: 'hidden',
+          transform: 'translateY(-30%)',
+        }),
+        animate(2000, style({
+          height: 'auto',
+          overflow: 'hidden',
+          transform: 'translateY(0%)',
+        }))
+      ]),
+    ])
+  ]
 })
 export class RegisterPage implements OnInit {
 
